f36112f50c6c25d664ef978f2bb35e44738f39aa
latch <latch@example.org>
2018-08-16 03:45:43 +0200

    forge latchkey 'git.suckless.org/st'

--- solarized-dark-config.h
+++ solarized-dark-config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "-xos4-terminus-medium-r-normal-*-16-*-*-*-*-*-iso10646-*";
+static char font[] = "Terminus:size=12";
 static int borderpx = 4;
 
 /*
